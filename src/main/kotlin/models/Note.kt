bc314f0e8b1c0d2b1795bd69f703b54e8afc9f6f
Troy <20098362@mail.wit.ie>
2023-03-07 09:37:08 +0000

    Add models package, add Note model

---
+++ src/main/kotlin/models/Note.kt
@@ -0,0 +1,8 @@
+package ie.setu.models
+
+data class Note (val noteTitle: String,
+                 val notePriority: Int,
+                 val noteCategory: String,
+                 val isNoteArchived: Boolean){
+
+}
