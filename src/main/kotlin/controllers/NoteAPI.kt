e3080a1319114390bf5dd27cba467f598aff2195
Troy <20098362@mail.wit.ie>
2023-03-15 13:51:05 +0000

    Add delete method + relevant tests

--- src/main/kotlin/controllers/NoteAPI.kt
+++ src/main/kotlin/controllers/NoteAPI.kt
@@ -118,4 +118,11 @@ class NoteAPI {
         return (index >= 0 && index < list.size)
     }
 
+    fun deleteNote(indexToDelete: Int): Note? {
+        return if (isValidListIndex(indexToDelete, notes)) {
+            notes.removeAt(indexToDelete)
+        } else null
+    }
+
+
 }
